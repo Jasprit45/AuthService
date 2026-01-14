9538518f77eb27998aebe330eb02f28a22047612
Jasprit45 <singhsardarjasprit@gmail.com>
2026-01-14 11:49:37 +0530

    signup logic with password encryption

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,7 +1,10 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
 
 dotenv.config();
+console.log(process.env.PORT);
 
 module.exports = {
     PORT: process.env.PORT,
+    SALT: bcrypt.genSaltSync(10),
 }
