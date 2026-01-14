c892a67008704faac40c51be03f5f6c2dd810096
Jasprit45 <singhsardarjasprit@gmail.com>
2026-01-14 19:15:42 +0530

    Added fuction to compare incoming password with encrypted password

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const UserRepository = require('../repository/user-repository');
 const {JWT_KEY} = require('../config/serverConfig');
+const bcrypt = require('bcrypt');
 
 class UserService {
     constructor() {
@@ -18,25 +19,34 @@ class UserService {
         }
         
     createToken(user) {
-            try {
-                const result = jwt.sign(user , JWT_KEY , {expiresIn: '1d'});
-                return result;
-            } catch (error) {
-                console.log("Something went wrong in token creation(service layer)");
-                // console.log(error);
-                throw(error);
-            }
+        try {
+            const result = jwt.sign(user , JWT_KEY , {expiresIn: '1d'});
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation(service layer)");
+            // console.log(error);
+            throw(error);
         }
+    }
         
-        verifyToken(token) {
-            try {
-                const result = jwt.verify(token,JWT_KEY);
-                return result;
-            } catch (error) {
-                console.log("Something went wrong in token verification(service layer)");
-                // console.log(error);
-                throw(error);
-            }
+    verifyToken(token) {
+        try {
+            const result = jwt.verify(token,JWT_KEY);
+            return result;
+        } catch (error) {
+            console.log("Something went wrong in token verification(service layer)");
+            // console.log(error);
+            throw(error);
+        }
+    }
+
+    checkPassword(userInputplainPassword , encryptedPassword) {
+        try {
+            return bcrypt.compareSync(userInputplainPassword,encryptedPassword);
+        } catch (error) {
+            console.log("Something went wrong in password check(service layer)");
+            throw(error);
+        }
     }
 }
 
